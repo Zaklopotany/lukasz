35f26df09be2705d0536b637da25627261b5a690
michal <marry.jane.kelly@gmail.com>
2019-01-28 20:20:53 +0100

    drugi

--- src/main/java/Ciezarowka.java
+++ src/main/java/Ciezarowka.java
@@ -5,7 +5,26 @@ public class Ciezarowka extends Pojazd {
     private Naczepa naczepa;
 
     public Ciezarowka() {
+        this.ladownosc = 3299;
+        this.model = "Model";
+        this.naczepa = new Naczepa();
     }
 
+    public Ciezarowka(String model, int ladownosc, Naczepa naczepa) {
+        this.model = model;
+        this.ladownosc = ladownosc;
+        this.naczepa = naczepa;
+    }
 
+    public String getModel() {
+        return model;
+    }
+
+    public int getLadownosc() {
+        return ladownosc;
+    }
+
+    public Naczepa getNaczepa() {
+        return naczepa;
+    }
 }
--- src/main/java/Pojazd.java
+++ src/main/java/Pojazd.java
@@ -1,6 +1,12 @@
 public class Pojazd {
 
 
+    private class Samochod extends Pojazd{
+        private String marka;
+        private int iloscDrzwi;
+        private Silnik slink;
+    }
+
 
 
 }
